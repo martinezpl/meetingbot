04cd3ae485645aa2be47e1645a0f13e05ee612b2
martinezpl <royfish@proton.me>
2025-05-07 14:34:56 +0200

    fix: ridiculous resources

--- src/terraform/ecs.tf
+++ src/terraform/ecs.tf
@@ -312,8 +312,8 @@ resource "aws_ecs_task_definition" "zoom_bot" {
   family                   = "${local.name}-zoom-bot"
   requires_compatibilities = ["FARGATE"]
   network_mode             = "awsvpc"
-  cpu                      = 4096
-  memory                   = 16384
+  cpu                      = 1024
+  memory                   = 2048
   execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
   task_role_arn            = aws_iam_role.bot_role.arn
 
@@ -354,8 +354,8 @@ resource "aws_ecs_task_definition" "teams_bot" {
   family                   = "${local.name}-teams-bot"
   requires_compatibilities = ["FARGATE"]
   network_mode             = "awsvpc"
-  cpu                      = 4096
-  memory                   = 16384
+  cpu                      = 1024
+  memory                   = 2048
   execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
   task_role_arn            = aws_iam_role.bot_role.arn
 
