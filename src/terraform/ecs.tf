848b0d9e24f8c5c9764490a5b0702897cdb7951c
martinezpl <royfish@proton.me>
2025-05-16 15:39:05 +0200

    bot performance

--- src/terraform/ecs.tf
+++ src/terraform/ecs.tf
@@ -312,8 +312,8 @@ resource "aws_ecs_task_definition" "zoom_bot" {
   family                   = "${local.name}-zoom-bot"
   requires_compatibilities = ["FARGATE"]
   network_mode             = "awsvpc"
-  cpu                      = 1024
-  memory                   = 2048
+  cpu                      = 2048
+  memory                   = 4096
   execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
   task_role_arn            = aws_iam_role.bot_role.arn
 
@@ -354,8 +354,8 @@ resource "aws_ecs_task_definition" "teams_bot" {
   family                   = "${local.name}-teams-bot"
   requires_compatibilities = ["FARGATE"]
   network_mode             = "awsvpc"
-  cpu                      = 1024
-  memory                   = 2048
+  cpu                      = 2048
+  memory                   = 4096
   execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
   task_role_arn            = aws_iam_role.bot_role.arn
 
